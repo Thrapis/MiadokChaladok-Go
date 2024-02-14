65a1bbccbd68dac87798604d2214118ddf68fb38
Thrapis <50047219+Thrapis@users.noreply.github.com>
2024-02-14 14:33:28 +0300

    File size reduction + content update

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 docker compose build --no-cache
+
 docker compose up
 
+---
 
 docker-compose up --build
--- database/migration.sql
+++ database/migration.sql
@@ -19,6 +19,74 @@ VALUES
   (
     N'Набор “Трыплет”',
     N'/static/images/products/product-set-1.jpg'
+  ),
+  (
+    N'Пярга',
+    N'/static/images/products/bee-pollen.jpg'
+  ),
+  (
+    N'Ажынавы мёд',
+    N'/static/images/products/blackberry-12oz.jpg'
+  ),
+  (
+    N'Лугавы мёд',
+    N'/static/images/products/buckwheat-honey-16oz.jpg'
+  ),
+  (
+    N'Мёд з іван-чая',
+    N'/static/images/products/fireweed-honey-12oz.jpg'
+  ),
+  (
+    N'Пальметто мёд',
+    N'/static/images/products/palmetto-honey-12oz.jpg'
+  ),
+  (
+    N'Канюшынавы мёд',
+    N'/static/images/products/pure-clover-honey-12oz.jpg'
+  ),
+  (
+    N'Журавінавы мёд',
+    N'/static/images/products/pure-cranberry-honey-16oz.jpg'
+  ),
+  (
+    N'Малінавы мёд',
+    N'/static/images/products/raspberry-honey-16oz.jpg'
+  ),
+  (
+    N'Грэчкавы мёд',
+    N'/static/images/products/sage-honey-16oz.jpg'
+  ),
+  (
+    N'Мёд кіслы',
+    N'/static/images/products/sourwood-honey-12oz.jpg'
+  ),
+  (
+    N'Праполіс "Навагодні"',
+    N'/static/images/products/tasmanian-christmas-bush-honey.jpg'
+  ),
+  (
+    N'Праполіс "Тасманскі"',
+    N'/static/images/products/tasmanian-leatherwood-honey-w.jpg'
+  ),
+  (
+    N'Тупело мёд',
+    N'/static/images/products/tupelo-honey-12oz.jpg'
+  ),
+  (
+    N'Рапсавы мёд',
+    N'/static/images/products/wildflower-honey-w-comb-1.jpg'
+  ),
+  (
+    N'Шэсць пакетаў мёду',
+    N'/static/images/products/honey-sampler-six-pack-1.jpg'
+  ),
+  (
+    N'Трыо аматараў гарачага мёду',
+    N'/static/images/products/hot-honey-gift-set.jpg'
+  ),
+  (
+    N'Цёплы і ўтульны мядовы набор',
+    N'/static/images/products/warm-cozy-honey-gift-set.jpg'
   );
   
 CREATE TABLE Options (
@@ -38,7 +106,43 @@ VALUES
   (1, N'500 мл', 20.00),
   (2, N'450 мл', 19.00),
   (2, N'650 мл', 24.00),
-  (3, N'3 банкі мёду + паштоўка', 30.00);
+  (3, N'3 банкі мёду + паштоўка', 30.00),
+  (4, N'100 мл', 12.00),
+  (4, N'350 мл', 15.00),
+  (4, N'500 мл', 20.00),
+  (5, N'450 мл', 19.00),
+  (5, N'650 мл', 24.00),
+  (6, N'100 мл', 12.00),
+  (6, N'350 мл', 15.00),
+  (6, N'500 мл', 20.00),
+  (7, N'450 мл', 19.00),
+  (7, N'650 мл', 24.00),
+  (8, N'100 мл', 12.00),
+  (8, N'350 мл', 15.00),
+  (8, N'500 мл', 20.00),
+  (9, N'100 мл', 12.00),
+  (9, N'350 мл', 15.00),
+  (9, N'500 мл', 20.00),
+  (10, N'450 мл', 19.00),
+  (10, N'650 мл', 24.00),
+  (11, N'450 мл', 19.00),
+  (11, N'650 мл', 24.00),
+  (12, N'450 мл', 19.00),
+  (12, N'650 мл', 24.00),
+  (13, N'100 мл', 12.00),
+  (13, N'350 мл', 15.00),
+  (13, N'500 мл', 20.00),
+  (14, N'450 мл', 19.00),
+  (14, N'650 мл', 24.00),
+  (15, N'450 мл', 19.00),
+  (15, N'650 мл', 24.00),
+  (16, N'450 мл', 19.00),
+  (16, N'650 мл', 24.00),
+  (17, N'450 мл', 19.00),
+  (17, N'650 мл', 24.00),
+  (18, N'6 слоік любога мёду', 70.00),
+  (19, N'3 слоікі вострага мёду', 38.00),
+  (20, N'3 слоікі любога мёду', 35.00);
 
 
 CREATE TABLE Recommendations (
